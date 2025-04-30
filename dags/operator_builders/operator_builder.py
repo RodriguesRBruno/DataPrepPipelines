bc6d7759ee55bd62d60a7481e4e2fe6f3864d327
Bruno Rodrigues <brunoresenderodrigues@gmail.com>
2025-04-30 15:32:56 -0300

    Refactor most of operator_factory.py into a class method for operator_builder.py

--- dags/operator_builders/operator_builder.py
+++ dags/operator_builders/operator_builder.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from copy import deepcopy
 from airflow.models import Pool
 from sqlalchemy.exc import OperationalError
+from constants import ALWAYS_CONDITION
 
 
 class OperatorBuilder(ABC):
@@ -92,3 +93,94 @@ class OperatorBuilder(ABC):
 
     def remove_next_id(self, next_id):
         self.next_ids.remove(next_id)
+
+    @classmethod
+    def build_operator_list(cls, **kwargs):
+        """
+        Helper method to build a list of required Operators for a DAG Builder.
+        Usually will return a list with a single element that is the desired operator
+        If conditional next_ids are sent from the YAML file, then this will return a list including
+        a Python Sensor Operator and a Python Branching Operator, which are both used to deal with branching
+        """
+        operator_list = []
+        kwargs["operator_id"] = kwargs.pop("id", None)
+
+        id_info = kwargs.pop("next", [])
+        conditions_definitions = kwargs.pop(
+            "conditions_definitions"
+        )  # [{'id': 'condition_1', 'type': 'function', 'function_name': 'function_name'}...]
+        conditions_definitions = {
+            condition["id"]: {
+                key: value for key, value in condition.items() if key != "id"
+            }
+            for condition in conditions_definitions
+        }  # {'condition_1: {'type': 'function', 'function_name': 'function_name'}, ...}
+
+        if isinstance(id_info, dict):
+            # If we have a branching condition in YAML, we return three operators:
+            # OperatorFromYAML -> PythonSensorOperator -> PythonBranchOperator -> EmptyOperator -> NextOperatorFromYAML
+            # OperatorFromYAML runs as defind by the YAML File.
+            # A PythonSensorOperator then waits for any of the defind conditions to be True and
+            # forwards the True condition to the PythonBranchOperator, which then branches accordingly.
+            # The Sensor and Branch Operators are defined here, so we can adapt the input arguments of the first operator accordingly
+            # (ie make it go into sensor that goes into branch which then goes into other operators from the YAML file)
+            # Empty operators are used between the branch operator and next operator from YAML to simplify breaking DAG cycles, if any
+            # are present. If DAG cycles are not present, the Empty operators do not interfere with DAG execution.
+            from .branch_from_sensor_operator_builder import (
+                BranchFromSensorOperatorBuilder,
+            )
+            from .python_sensor_builder import PythonSensorBuilder
+            from .empty_operator_builder import EmptyOperatorBuilder
+
+            branching_info: list[dict[str, str]] = id_info.pop("if")
+            sensor_id = f'sensor_from_{kwargs["operator_id"]}'
+            branching_id = f'branch_from_{kwargs["operator_id"]}'
+            wait_time = id_info.pop("wait", None)
+            default_condition = id_info.pop("else", None)
+            kwargs["next_ids"] = sensor_id
+
+            conditions = branching_info
+            if default_condition and default_condition != kwargs["operator_id"]:
+                conditions.append(
+                    {"condition": ALWAYS_CONDITION, "target": default_condition}
+                )
+            processed_conditions = []
+            for condition in conditions:
+                processed_condition = {
+                    "condition": condition["condition"],
+                    "target": f"empty_before_{condition['target']}",
+                }
+                processed_conditions.append(processed_condition)
+
+            empty_ids = [condition["target"] for condition in processed_conditions]
+            ids_after_empty = [condition["target"] for condition in conditions]
+
+            sensor_operator = PythonSensorBuilder(
+                conditions=processed_conditions,
+                wait_time=wait_time,
+                operator_id=sensor_id,
+                next_ids=[branching_id],
+                conditions_definitions=conditions_definitions,
+                from_yaml=False,
+            )
+
+            empty_operators = [
+                EmptyOperatorBuilder(
+                    operator_id=empty_id, next_ids=[next_id], from_yaml=False
+                )
+                for empty_id, next_id in zip(empty_ids, ids_after_empty)
+            ]
+
+            branch_operator = BranchFromSensorOperatorBuilder(
+                next_ids=[empty_id for empty_id in empty_ids],
+                previous_sensor=sensor_operator,
+                operator_id=branching_id,
+                from_yaml=False,
+            )
+            operator_list.extend([sensor_operator, branch_operator, *empty_operators])
+        else:
+            kwargs["next_ids"] = id_info
+
+        this_operator = cls(**kwargs)
+        operator_list.append(this_operator)
+        return operator_list
--- dags/operator_factory.py
+++ dags/operator_factory.py
@@ -1,100 +1,23 @@
 from __future__ import annotations
-from operator_builders.branch_from_sensor_operator_builder import (
-    BranchFromSensorOperatorBuilder,
-)
 from operator_builders.docker_operator_buider import DockerOperatorBuilder
 from operator_builders.empty_operator_builder import EmptyOperatorBuilder
 from operator_builders.manual_approval_buider import ManualApprovalBuilder
 from operator_builders.operator_builder import OperatorBuilder
-from operator_builders.python_sensor_builder import PythonSensorBuilder
-from constants import ALWAYS_CONDITION
+
+OPERATOR_MAPPING: dict[str, OperatorBuilder] = {
+    "container": DockerOperatorBuilder,
+    "dummy": EmptyOperatorBuilder,
+    "manual_approval": ManualApprovalBuilder,
+}
 
 
 def operator_factory(type, **kwargs) -> list[OperatorBuilder]:
 
     return_list = []
-    kwargs["operator_id"] = kwargs.pop("id", None)
-    id_info = kwargs.pop("next", [])
-    conditions_definitions = kwargs.pop(
-        "conditions_definitions"
-    )  # [{'id': 'condition_1', 'type': 'function', 'function_name': 'function_name'}...]
-    conditions_definitions = {
-        condition["id"]: {key: value for key, value in condition.items() if key != "id"}
-        for condition in conditions_definitions
-    }  # {'condition_1: {'type': 'function', 'function_name': 'function_name'}, ...}
-
-    if isinstance(id_info, dict):
-        # If we have a branching condition in YAML, we return three operators:
-        # OperatorFromYAML -> PythonSensorOperator -> PythonBranchOperator -> EmptyOperator -> NextOperatorFromYAML
-        # OperatorFromYAML runs as defind by the YAML File.
-        # A PythonSensorOperator then waits for any of the defind conditions to be True and
-        # forwards the True condition to the PythonBranchOperator, which then branches accordingly.
-        # The Sensor and Branch Operators are defined here, so we can adapt the input arguments of the first operator accordingly
-        # (ie make it go into sensor that goes into branch which then goes into other operators from the YAML file)
-        # Empty operators are used between the branch operator and next operator from YAML to simplify breaking DAG cycles, if any
-        # are present. If DAG cycles are not present, the Empty operators do not interfere with DAG execution.
-
-        branching_info: list[dict[str, str]] = id_info.pop("if")
-        sensor_id = f'sensor_from_{kwargs["operator_id"]}'
-        branching_id = f'branch_from_{kwargs["operator_id"]}'
-        wait_time = id_info.pop("wait", None)
-        default_condition = id_info.pop("else", None)
-        kwargs["next_ids"] = sensor_id
-
-        conditions = branching_info
-        if default_condition and default_condition != kwargs["operator_id"]:
-            conditions.append(
-                {"condition": ALWAYS_CONDITION, "target": default_condition}
-            )
-        processed_conditions = []
-        for condition in conditions:
-            processed_condition = {
-                "condition": condition["condition"],
-                "target": f"empty_before_{condition['target']}",
-            }
-            processed_conditions.append(processed_condition)
-
-        empty_ids = [condition["target"] for condition in processed_conditions]
-        ids_after_empty = [condition["target"] for condition in conditions]
-
-        sensor_operator = PythonSensorBuilder(
-            conditions=processed_conditions,
-            wait_time=wait_time,
-            operator_id=sensor_id,
-            next_ids=[branching_id],
-            conditions_definitions=conditions_definitions,
-            from_yaml=False,
-        )
-
-        empty_operators = [
-            EmptyOperatorBuilder(
-                operator_id=empty_id, next_ids=[next_id], from_yaml=False
-            )
-            for empty_id, next_id in zip(empty_ids, ids_after_empty)
-        ]
-
-        branch_operator = BranchFromSensorOperatorBuilder(
-            next_ids=[empty_id for empty_id in empty_ids],
-            previous_sensor=sensor_operator,
-            operator_id=branching_id,
-            from_yaml=False,
-        )
-        return_list.extend([sensor_operator, branch_operator, *empty_operators])
-
-    else:
-        kwargs["next_ids"] = id_info
-
-    if type == "container":
-        # TODO different check (env config?) for docker vs singularity, for now just docker:
-        return_list.append(DockerOperatorBuilder(**kwargs))
-
-    elif type == "manual_approval":
-        return_list.append(ManualApprovalBuilder(**kwargs))
-
-    elif type == "dummy":
-        return_list.append(EmptyOperatorBuilder(**kwargs))
-
-    else:
+    try:
+        operator_obj = OPERATOR_MAPPING[type]
+    except KeyError:
         raise TypeError(f"Tasks of type {type} are not supported!")
 
+    return_list = operator_obj.build_operator_list(**kwargs)
     return return_list
